b25e363da92e3688779367bc9bd4a18aecbdbb09
Biswanath Tewari <biswanath@geekyants.com>
2022-06-05 23:11:04 +0530

    feat: ✨ history support

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
 
 import Navigation from './navigation'
+import { history } from './services'
 import { store } from './store'
 import './styles.scss'
 
 export const App = () => (
-  <Router>
+  <HistoryRouter history={history}>
     <Provider store={store}>
       <ChakraProvider theme={theme}>
         <Navigation />
       </ChakraProvider>
     </Provider>
-  </Router>
+  </HistoryRouter>
 )
