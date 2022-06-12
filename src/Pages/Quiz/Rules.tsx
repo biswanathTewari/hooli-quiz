f6b668a02ca7e4f6f9ad926468ccc9e3885234f5
Biswanath Tewari <biswanath@geekyants.com>
2022-06-12 21:22:33 +0530

    feat: ✨ rules api call

--- src/Pages/Quiz/Rules.tsx
+++ src/Pages/Quiz/Rules.tsx
@@ -10,13 +10,19 @@ import {
 } from '@chakra-ui/react'
 
 import { DefaultImg } from '../../assets/images'
-import { PrimaryButton } from '../../components'
+import { PrimaryButton, Loader } from '../../components'
+import { Quiz } from '../../services'
 
-const Rules = () => {
+interface Props extends Omit<Quiz, 'questions'> {
+  loading: boolean
+}
+
+const Rules = ({ info, loading }: Props) => {
+  if (loading) return <Loader />
   return (
     <Box height={'100%'} width={'100%'} flexDir={'column'}>
       <Text fontSize={['x-large', 'xx-large', 'xx-large']} fontWeight="700">
-        History Quiz
+        {info.title}
       </Text>
       <Text fontSize={['medium', 'large', 'large']} fontWeight="400">
         Read the following instructions
@@ -33,11 +39,11 @@ const Rules = () => {
           overflow="hidden"
         >
           <Image
-            src={''}
+            src={info.image}
             alt="category"
             objectFit="initial"
-            height={'13rem'}
-            width={'20rem'}
+            height={'100%'}
+            width={'100%'}
           />
         </Box>
         <Grid
@@ -81,14 +87,14 @@ const Rules = () => {
               fontWeight="400"
               mb={'1rem'}
             >
-              25/06/1999
+              26/06/2022
             </Text>
             <Text
               fontSize={['small', 'small', 'large', 'large']}
               fontWeight="400"
               mb={'1rem'}
             >
-              30mins
+              {info.time}
             </Text>
             <Text
               fontSize={['small', 'small', 'large', 'large']}
@@ -102,7 +108,7 @@ const Rules = () => {
               fontWeight="400"
               mb={'1rem'}
             >
-              100 points
+              {Number(info.questionsCount) * 10} points
             </Text>
           </GridItem>
         </Grid>
@@ -115,13 +121,13 @@ const Rules = () => {
         Instructions
       </Text>
       <Text fontSize={'md'} fontWeight="400">
-        This quiz consists of 5 multiple-choice questions. To be successful with
+        {`This quiz consists of ${info.questionsCount} multiple-choice questions. To be successful with
         the quizzes, its important to conversant with the topics. Keep the
-        following in mind:
+        following in mind:`}
       </Text>
       <Text fontSize={'md'} fontWeight="400" m={'1rem 0'}>
-        Timing - You need to complete each of your attempts in one sitting, as
-        you are allotted 30 minutes to each attempt.
+        {`Timing - You need to complete each of your attempts in one sitting, as
+        you are allotted ${info.time} minutes to each attempt.`}
       </Text>
       <Text fontSize={'md'} fontWeight="400">
         Answers - You may review your answer-choices and compare them to the
--- src/Pages/Quiz/index.tsx
+++ src/Pages/Quiz/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import { Box, useColorModeValue } from '@chakra-ui/react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import Rules from './Rules'
 import { Navbar } from '../../components'
+import { getQuizAction, getQuizInfo, getQuizIsLoading } from '../../store'
 
 const Quiz = () => {
+  const dispatch = useDispatch()
+  const { id } = useParams()
   const bgColor = useColorModeValue('white', '#282f3c')
   const containerColor = useColorModeValue('gray.50', '')
   const [mode] = React.useState<'rules' | 'quiz' | 'results'>('rules')
+  const info = useSelector(getQuizInfo)
+  const isLoading = useSelector(getQuizIsLoading)
+
+  React.useEffect(() => {
+    dispatch({ type: getQuizAction, payload: { id } })
+  }, [])
 
   return (
     <Box px={10} py={3.5} height={'100vh'} bg={containerColor}>
@@ -21,7 +32,7 @@ const Quiz = () => {
         padding={'1.5rem 2.5rem'}
         bg={bgColor}
       >
-        {mode === 'rules' && <Rules />}
+        {mode === 'rules' && <Rules info={info} loading={isLoading} />}
       </Box>
     </Box>
   )
